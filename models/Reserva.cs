d6128ed91165c59dc42b03d95b7e5ebd2cedaf3c
AndreCampeloR <decocampeloreis@gmail.com>
2023-01-31 13:36:12 -0300

    calculando valor da diária

--- models/Reserva.cs
+++ models/Reserva.cs
@@ -40,20 +40,16 @@ namespace SistemaHotel_Dio.models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidadeDeHospedes = Hospedes.Count;
+
+            return quantidadeDeHospedes;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (true)
             {
                 valor = 0;
